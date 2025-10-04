2e8dc2406b1ec97f8f9ad908c29fdf73d7c1ed27
Callum Birks <callumbirks@protonmail.com>
2025-10-04 15:51:54 +0100

    Add BLOCK_ENTITY_LOAD/UNLOAD events.

--- src/main/kotlin/com/doofcraft/vessel/api/VesselBlock.kt
+++ src/main/kotlin/com/doofcraft/vessel/api/VesselBlock.kt
@@ -9,7 +9,6 @@ import net.minecraft.server.level.ServerPlayer
 import net.minecraft.world.InteractionHand
 import net.minecraft.world.InteractionResult
 import net.minecraft.world.entity.LivingEntity
-import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.ItemLike
 import net.minecraft.world.level.LevelAccessor
 
